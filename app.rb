376c7576eb456e87fcd328d25b36193455357af2
Derek Chen <derekgc@icloud.com>
2018-05-14 17:52:30 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
+# require "pry"
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+
+  post '/piglatinize' do
+    text_from_user = params[:user_phrase]
+
+    @pigLatin_text = PigLatinizer.new(text_from_user)
+    # binding.pry
+
+    erb :results
+  end
 end
