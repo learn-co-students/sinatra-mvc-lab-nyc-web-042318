76cbfa51ea012aad15d2b47a26e7422c6a4214ff
Canino Developments <ian.canino@gmail.com>
2018-05-19 12:33:55 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
-
+# This class controls user input and translates to pig output using model
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    pl = PigLatinizer.new
+    @piglatin = pl.piglatinize(params[:user_phrase])
+    erb :results
+  end
 end
